47cc19f4d76d7f832d612be52aa962bc6cf75631
Karlo-wws <info@wipingwithstyleguild.com>
2014-09-16 16:29:51 -0700

    removing print

--- itempost.py
+++ itempost.py
@@ -14,7 +14,7 @@ if numcheck > 0:
         data = x.fetchall()
         for row in data:
                 charname = row[0]
-                #enc = str(charname)
+                # enc = str(charname)
                 itemid = str(row[1])
                 stamp = str(row[2])
                 URL = Host + "/api/wow/item/" + itemid
@@ -22,7 +22,7 @@ if numcheck > 0:
                 c = r.json()
                 itemname = c['name']
                 print charname
-                print enc
+                # print enc
                 print itemid
                 print stamp
                 POST = '%s acquired %s http://www.wowhead.com/item=%s #loot #worldofwarcraft' % (charname,itemname,itemid)
